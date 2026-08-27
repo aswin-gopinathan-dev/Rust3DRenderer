777bf51abed3ec7a189883edff4d43b77dd63df1
Aswin Gopinathan <aswin.gopinathan@outlook.com>
2026-08-27 00:18:52 -0300

    Color Buffer

---
+++ Rust3DRenderer/src/display.rs
@@ -0,0 +1,97 @@
+use sdl2::video::Window;
+use sdl2::video::FullscreenType;
+use sdl2::render::Canvas;
+use sdl2::Sdl;
+
+pub struct AppWindow {
+    pub event_pump: sdl2::EventPump,
+    pub canvas: Canvas<Window>,
+    pub is_running: bool,
+    pub color_buffer: Vec<u32>,
+    screen_width: u32,
+    screen_height: u32
+}
+
+impl AppWindow {
+    pub fn new(sdl_context: &Sdl, video_subsystem: &sdl2::VideoSubsystem) -> Result<Self, String> {
+        let display_mode = video_subsystem.current_display_mode(0)
+                                          .map_err(|e| e.to_string())?;
+
+        let screen_width = display_mode.w as u32;
+        let screen_height = display_mode.h as u32;
+
+        let mut window = video_subsystem.window("3D Renderer", screen_width, screen_height)
+                                     .position_centered()
+                                     .build()
+                                     .map_err(|e| format!("error while initializing window. {e}"))?;
+
+        window.set_fullscreen(FullscreenType::Desktop)
+              .map_err(|e| e.to_string())?;        
+
+        let event_pump = sdl_context.event_pump()?;
+
+        let canvas = window.into_canvas()
+                            .build()
+                            .map_err(|e| format!("error while initializing canvas. {e}"))?;
+
+
+        Ok( Self {
+                event_pump,
+                canvas,
+                is_running: true,
+                color_buffer: vec![0; (screen_width * screen_height) as usize],
+                screen_width,
+                screen_height
+        })
+    }
+
+    pub fn render(&mut self) -> Result<(), String> {
+        self.canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 0));
+        self.canvas.clear();
+
+        self.clear_color_buffer(0xFF000000);
+        self.draw_grid(0xFFFFFFFF);
+        self.render_color_buffer()?;        
+
+        self.canvas.present();
+
+        Ok(())
+    }
+
+    pub fn render_color_buffer(&mut self) -> Result<(), String> {
+        let texture_creator = self.canvas.texture_creator();
+        let mut texture = texture_creator.create_texture_streaming(sdl2::pixels::PixelFormatEnum::ARGB8888, 
+                                                                   self.screen_width, self.screen_height)
+                                         .map_err(|e| e.to_string())?;
+
+        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                for y in 0..self.screen_height as usize {
+                    for x in 0..self.screen_width as usize {
+                        let pixel = self.color_buffer[y * self.screen_width as usize + x];
+                        let offset = y * pitch + x * 4;
+                        buffer[offset..offset + 4].copy_from_slice(&pixel.to_ne_bytes());
+                }
+            }
+        }).map_err(|e| e.to_string())?;
+
+        self.canvas.copy(&texture, None, None)
+                   .map_err(|e| e.to_string())?;
+
+        Ok(())
+    }
+
+    pub fn clear_color_buffer(&mut self, color: u32){
+        self.color_buffer.fill(color);
+    }
+
+    pub fn draw_grid(&mut self, color: u32) {
+        for y in 0..self.screen_height {
+            for x in 0..self.screen_width {
+                if y % 50 == 0 || x % 50 == 0 {
+                    let index = (self.screen_width * y + x) as usize;
+                    self.color_buffer[index] = color;
+                }
+            }
+        } 
+    }
+}
--- Rust3DRenderer/src/main.rs
+++ Rust3DRenderer/src/main.rs
@@ -1,59 +1,39 @@
-use sdl2::video::Window;
-use sdl2::render::Canvas;
+
 use sdl2::event::Event;
-use sdl2::Sdl;
+use sdl2::keyboard::Keycode;
+
+mod display;
+
+use display::AppWindow;
 
 
 
-const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 600;
-
-struct AppWindow {
-    event_pump: sdl2::EventPump,
-    canvas: Canvas<Window>,
-    is_running: bool
-}
-
-impl AppWindow {
-    fn new(sdl_context: &Sdl, video_subsystem: &sdl2::VideoSubsystem) -> Result<Self, String> {
-        let window = video_subsystem.window("3D Renderer", SCREEN_WIDTH, SCREEN_HEIGHT)
-                                     .position_centered()
-                                     .build()
-                                     .map_err(|e| format!("error while initializing window. {e}"))?;
-
-        let event_pump = sdl_context.event_pump()?;
-
-        let canvas = window.into_canvas()
-                            .build()
-                            .map_err(|e| format!("error while initializing canvas. {e}"))?;
-
-        Ok( Self {
-                event_pump,
-                canvas,
-                is_running: true,
-        })
-    }
-
-    fn process_input(&mut self) {
-        for event in self.event_pump.poll_iter() {
-            if let Event::Quit { .. } = event {
-                self.is_running = false;
+fn process_input(wnd: &mut AppWindow) {
+    for event in wnd.event_pump.poll_iter() {
+        match event {
+            Event::Quit { .. } => {
+                println!("Quit event");
+                wnd.is_running = false;
             }
-        }
-    }
 
-    fn setup() {
-    }
+            Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => {
+                wnd.is_running = false;
+            }
 
-    fn update() {
-    }
-
-    fn render(&mut self) {
-        self.canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 0));
-        self.canvas.clear();
-        self.canvas.present();
+            _ => {}
+        } 
     }
 }
+
+fn setup() {
+}
+
+fn update() {
+}
+
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -61,11 +41,11 @@ fn main() -> Result<(), String> {
 
     let mut app_window = AppWindow::new(&sdl_context, &video_subsystem)?;
 
-    AppWindow::setup();
+    setup();
     while app_window.is_running {
-        app_window.process_input();
-        AppWindow::update();
-        app_window.render();
+        process_input(&mut app_window);
+        update();
+        app_window.render()?;
     }
 
     Ok(())
